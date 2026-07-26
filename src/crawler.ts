7e187b10467aa3827c9bb4515e93fa839dce3b83
SusanAlexFSD <susan.alexanderfsd@gmail.com>
2026-07-26 21:40:43 +0100

    changes made to crawler

--- src/crawler.ts
+++ src/crawler.ts
@@ -176,6 +176,13 @@ export async function crawlWebsite(
       hasSitemap = false;
     }
 
+const screenshotBytes = await page.screenshot({
+  type: "png",
+  fullPage: true,
+});
+
+const screenshot = `data:image/png;base64,${Buffer.from(screenshotBytes).toString("base64")}`;
+
     const result: CrawlSuccess = {
       title,
       pageUrl,
@@ -199,7 +206,7 @@ export async function crawlWebsite(
       hasRobots,
       hasSitemap,
 
-      screenshot: "",
+      screenshot,
     };
 
     return result;
